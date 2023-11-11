5b8e10ed3d4ddfbb8023b6015d7dd3d9ce76ba5a
AVeliz <by.veliz.alejandra@gmail.com>
2023-11-10 20:49:13 -0500

    Added new folder

---
+++ Equipo N°02 – CodigoFuente/main.cpp
@@ -0,0 +1,13 @@
+/* Equipo N°02 – CodigoFuente.cpp
+    Jesús Augusto Reyes Mechato	[2210838]  
+    Alejandra, Veliz García		[2311640]  
+    Jordán F. Rodríguez Sánchez	[2111666]  
+    Nicholas, Yarrow Larrauri	[2312374] 
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "LUDO\n";
+}
+
